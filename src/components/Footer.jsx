c458ebba6f433f22406f99b99818eb6deac12602
Okolosiokaro <okolosiokaro@gmail.com>
2024-04-01 04:22:23 +0100

    staged changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
   return (
     <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
       <div>
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">React</h1>
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">EO</h1>
       <p className='py-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, consequuntur.</p>
       <div className='flex justify-between md:w-[75%] my-6'>
         <FaFacebookSquare size={30}/>
