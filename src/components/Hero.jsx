bff3afe003dee228894aaef767c9f7e1105f81d0
Okolosiokaro <okolosiokaro@gmail.com>
2024-04-01 05:16:37 +0100

    staged changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { ReactTyped } from "react-typed";
 
 const Hero = () => {
   return (
-    <div className="text-white">
+    <div className=''>
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
        <p className="text-[#00df9a] font-bold p-2">Hi my name is</p>
        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-4">OKOLOSIO, Oghenekaro Elvis</h1>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const handleNav = () => {
     setNav(!nav)
 }
   return (
-    <div className="flex justify-between  items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+    <div className="flex justify-between  items-center h-24 max-w-[1240px] mx-auto px-4">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">EO</h1>
       <ul className="hidden md:flex">
         <li className="p-4">Home</li>
@@ -20,15 +20,24 @@ const handleNav = () => {
       <div onClick={handleNav} className="block md:hidden">
       {nav ? <AiOutlineClose size={20} /> :  <AiOutlineMenu size={20}/> }
       </div>
-      <div className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%] " }>
-      <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">EO</h1>
+
+  
+      <div  className={nav ? "fixed left-0 top-0 w-full h-full bg-black/50" : "fixed -left-full " }>
+      <div className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-white ease-in-out duration-500" : "fixed -left-full " }>
+        <div className="flex justify-between items-center ">
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">EO</h1>
+        <div onClick={handleNav} className='p-4'>
+        {<AiOutlineClose size={20} />}
+        </div>
+        </div>
         <ul className="p-4 uppercase ">
-        <li className="p-4 border-b border-gray-600">Home</li>
-        <li className="p-4 border-b border-gray-600">Company</li>
-        <li className="p-4 border-b border-gray-600">Resources</li>
-        <li className="p-4 border-b border-gray-600">About</li>
-        <li className="p-4">Contact</li>
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">Company</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
+          <li className="p-4 border-b border-gray-600">About</li>
+          <li className="p-4">Contact</li>
         </ul>
+      </div>
       </div>
     </div>
   )
