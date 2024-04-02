555375917ab57df50a22beef22eaaaaf55b4e297
Okolosiokaro <okolosiokaro@gmail.com>
2024-04-02 03:45:27 +0100

    project updated changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
-import Project from "./components/Project";
+import Project from './components/Project'
 import Contact from './components/Contact';
 import Footer from "./components/Footer";
 import Skill from "./components/Skill";
