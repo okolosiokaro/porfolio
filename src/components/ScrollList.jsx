4f31f4e8ed6c40d5a3011ce870f162c26a76f920
Okolosiokaro <okolosiokaro@gmail.com>
2024-04-06 08:07:55 +0100

    staged changes

--- src/components/ScrollList.jsx
+++ src/components/ScrollList.jsx
@@ -23,17 +23,14 @@ const ScrollList = ({ Project }) => {
   return (
     <div>
        <div className="relative flex justify-center items-center">
-      <button
-        className="absolute left-0 top-1/2 bg-blue-400 hover:bg-blue-500 p-2 rounded-full z-10 text-white"
-        onClick={scrollToLeft}
-      >
+      <button className="absolute left-0 top-1/2 bg-blue-400 hover:bg-blue-500 p-2 rounded-full z-10 text-white" onClick={scrollToLeft}>
         &lt;
       </button>
       <div id="scrollContainer" className="flex overflow-x-auto" style={{ scrollBehavior: 'smooth' }}>
         {Projects.map((Project) => (
-                <div key={Project.id}  className="flex-none w-full md:w-1/2 flex flex-col justify-center items-center border border-gray-300 m-2">
+                <div key={Project.id}  className="flex-none w-full lg:w-[500px] md:w-1/2 flex flex-col justify-center items-center border border-gray-300 m-2">
                 <div className='mx-auto'>
-                 <img alt='/' src={Project.image}/>
+                 <img alt='/' src={Project.image} className=''/>
                 </div>
                 <h2 className='text-xl font-bold text-center py-4'>{Project.title}</h2>
                 <p className='text-center'>{Project.description}</p>
